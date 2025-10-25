bf055a17e9836dd968bf7f9cbaaea13e39867c65
PITICALYN <lpiticalyn@gmail.com>
2025-10-25 10:51:52 -0300

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,8 @@
-import { createClient } from '@supabase/supabase-js';
+// lib/supabaseClient.ts
+import { createClient } from '@supabase/supabase-js'
 
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-export const supabase = createClient(url, anon);
+// Criar a conexão com o Supabase
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
